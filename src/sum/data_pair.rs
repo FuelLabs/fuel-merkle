039e3b48c2ba5fe3191821988a9277cbb3cc2dc4
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-06-19 17:36:49 -0400

    Remove generics from Merkle sum tree

--- src/sum/data_pair.rs
+++ src/sum/data_pair.rs
@@ -1,20 +1,18 @@
-use byteorder::{BigEndian, ReadBytesExt};
-use std::io::Cursor;
+use std::convert::TryFrom;
 
 pub fn join_data_pair(fee: u64, data: &[u8]) -> Vec<u8> {
-    let mut sum_data = Vec::<u8>::new();
-    for d in fee.to_be_bytes().iter() {
-        sum_data.push(*d)
-    }
-    for d in data.iter() {
-        sum_data.push(*d)
-    }
+    let mut sum_data = Vec::<u8>::with_capacity(std::mem::size_of::<u64>() + data.len());
+    sum_data.extend_from_slice(&fee.to_be_bytes());
+    sum_data.extend_from_slice(data);
     sum_data
 }
 
 pub fn split_data_pair(data_pair: &[u8]) -> (u64, &[u8]) {
-    let fee_data = &data_pair[0..8];
-    let mut reader = Cursor::new(fee_data);
-    let fee = reader.read_u64::<BigEndian>().unwrap();
-    (fee, &data_pair[8..])
+    assert!(data_pair.len() > 8);
+
+    let (l, r) = data_pair.split_at(8);
+    let fee = <[u8; 8]>::try_from(l).unwrap();
+    let fee = u64::from_be_bytes(fee);
+
+    (fee, r)
 }
--- src/sum/hash.rs
+++ src/sum/hash.rs
@@ -1,44 +1,36 @@
-use crate::digest::Digest;
+use generic_array::GenericArray;
+use sha2::{Digest, Sha256 as Hash};
+// use digest::Digest; TODO: Use this instead of sha2::Digest once other PR is merged
 
-use std::convert::TryFrom;
+const NODE: u8 = 0x01;
+const LEAF: u8 = 0x00;
 
-const NODE: [u8; 1] = [0x01];
-const LEAF: [u8; 1] = [0x00];
-
-type Data = [u8; 32];
+pub type Data = GenericArray<u8, <Hash as Digest>::OutputSize>;
 
 // Merkle Tree hash of an empty list
 // MTH({}) = Hash()
-pub fn empty_sum<D: Digest>() -> Data {
-    let hash = D::new();
-    let data = hash.finalize();
-
-    <Data>::try_from(data.as_slice()).unwrap()
+pub fn empty_sum() -> Data {
+    let hash = Hash::new();
+    hash.finalize()
 }
 
 // Merkle tree hash of an n-element list D[n]
 // MTH(D[n]) = Hash(0x01 || LHS fee || MTH(D[0:k]) || RHS fee || MTH(D[k:n])
-pub fn node_sum<D: Digest>(lhs_fee: u64, lhs_data: &[u8], rhs_fee: u64, rhs_data: &[u8]) -> Data {
-    let mut hash = D::new();
-
-    hash.update(&NODE);
+pub fn node_sum(lhs_fee: u64, lhs_data: &[u8], rhs_fee: u64, rhs_data: &[u8]) -> Data {
+    let mut hash = Hash::new();
+    hash.update(&[NODE]);
     hash.update(lhs_fee.to_be_bytes());
     hash.update(&lhs_data);
     hash.update(rhs_fee.to_be_bytes());
     hash.update(&rhs_data);
-    let data = hash.finalize();
-
-    <Data>::try_from(data.as_slice()).unwrap()
+    hash.finalize()
 }
 
 // Merkle tree hash of a list with one entry
 // MTH({d(0)}) = Hash(0x00 || d(0))
-pub fn leaf_sum<D: Digest>(data: &[u8]) -> Data {
-    let mut hash = D::new();
-
-    hash.update(&LEAF);
+pub fn leaf_sum(data: &[u8]) -> Data {
+    let mut hash = Hash::new();
+    hash.update(&[LEAF]);
     hash.update(&data);
-    let data = hash.finalize();
-
-    <Data>::try_from(data.as_slice()).unwrap()
+    hash.finalize()
 }
