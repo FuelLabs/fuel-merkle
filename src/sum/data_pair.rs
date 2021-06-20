599b32dce5347890d9447743198fe72b1122b98d
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-06-19 23:53:54 -0400

    assert sizes in join and split data pair

--- src/sum/data_pair.rs
+++ src/sum/data_pair.rs
@@ -1,6 +1,8 @@
 use std::convert::TryFrom;
 
 pub fn join_data_pair(fee: u64, data: &[u8]) -> [u8; 40] {
+    assert_eq!(data.len(), 32);
+
     let mut sum_data = [0u8; 40];
     sum_data[0..8].copy_from_slice(&fee.to_be_bytes());
     sum_data[8..].copy_from_slice(data);
@@ -8,6 +10,8 @@ pub fn join_data_pair(fee: u64, data: &[u8]) -> [u8; 40] {
 }
 
 pub fn split_data_pair(data_pair: &[u8]) -> (u64, &[u8]) {
+    assert_eq!(data_pair.len(), 40);
+
     let (l, r) = data_pair.split_at(8);
     let fee = <[u8; 8]>::try_from(l).unwrap();
     let fee = u64::from_be_bytes(fee);
