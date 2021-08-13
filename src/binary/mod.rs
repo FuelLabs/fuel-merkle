98e64ddd6112f1e2c5c76bbae0ede0817791b9a2
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-08-13 17:26:29 -0400

    Update mod.rs

--- src/binary/mod.rs
+++ src/binary/mod.rs
@@ -1,8 +1,9 @@
 pub mod merkle_tree;
+pub mod storage;
+pub mod storage_map;
+pub mod storage_merkle_tree;
 pub mod verify;
 
 mod hash;
 mod node;
-mod storage;
-mod storage_map;
-mod storage_merkle_tree;
+
