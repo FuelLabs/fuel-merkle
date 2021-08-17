f4d3dfdfc43126f53e136176b441b93e2b35e5a7
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-08-17 13:36:52 -0400

    Update comments

--- src/common/position.rs
+++ src/common/position.rs
@@ -1,16 +1,15 @@
-/// Position
+/// #Position
 ///
 /// A `Position` represents a node's position in a binary tree by encapsulating the node's index
 /// data. Indices are calculated through in-order traversal of the nodes, starting with the first
 /// leaf node. Indexing starts at 0.
 ///
+/// ##Merkle Trees
+///
 /// In the context of Merkle trees, trees are constructed "upwards" from leaf nodes. Therefore,
 /// traversal is done from the bottom up, starting with the leaves, rather than top down, starting
-/// with the root, and we can guarantee a deterministic construction of index data. Furthermore,
-/// this means we can deterministically calculate related positional attributes, such as height,
-/// in constant time.
+/// with the root, and we can guarantee a deterministic construction of index data.
 ///
-/// In-order indices:
 /// ```text
 ///               07
 ///              /  \
@@ -33,19 +32,48 @@
 /// bottom row. Because of the in-order traversal used to calculate the in-order indices, leaf nodes
 /// have the property that their in-order index is always equal to their leaf index multiplied by 2.
 ///
-/// Leaf indices:
 /// ```text
-///  /  \    /  \    /  \    /  \
-/// 00  01  02  03  04  05  06  07
+///                    /  \    /  \    /  \    /  \
+///     Leaf indices: 00  01  02  03  04  05  06  07
+/// In-order indices: 00  02  04  06  08  10  12  14
 /// ```
 ///
-/// A `Position` exposes methods to traverse the binary tree.
+/// This allows us to construct a `Position` (and its in-order index) by providing either an
+/// in-order index directly or, in the case of a leaf, a leaf index. This functionality is captured
+/// by `from_in_order_index()` and `from_leaf_index()` respectively.
+///
+/// Traversal of a Merkle Tree can be performed by the methods on a given `Position` to retrieve its
+/// sibling, parent, or uncle `Position`.
+///
+/// ##Merkle Mountain Ranges
+///
+/// Because the `Position` indices are calculated from in-order traversal starting with the leaves,
+/// the deterministic quality of the indices holds true for imbalanced binary trees, including
+/// Merle Mountain Ranges. Consider the following binary tree construction comprised of seven
+/// leaves (with leaf indices 0 through 7):
+///
+/// ```text
+///       03
+///      /  \
+///     /    \
+///   01      05      09
+///  /  \    /  \    /  \
+/// 00  02  04  06  08  10  12
+/// ```
+///
+/// Note the absence of internal nodes that would be present in a fully balanced tree: inner nodes
+/// with indices 7, 11, and 13 are absent. This is owing to the fact that nodes indices are
+/// calculated deterministically through in-order traversal, not calculated as a sequence.
+///
+/// Traversal of a Merkle Mountain Range is still done in the same manner as a balanced Merkle tree,
+/// using methods to retrieve a `Position's` sibling, parent, or uncle `Position`. However, in such
+/// cases, the corresponding sibling or uncle nodes are not guaranteed to exist in the tree.
 ///
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Position(u64);
 
 impl Position {
-    pub fn index(self) -> u64 {
+    pub fn in_order_index(self) -> u64 {
         self.0
     }
 
@@ -64,7 +92,7 @@ impl Position {
     /// A position shares the same parent and height as its sibling.
     pub fn sibling(self) -> Self {
         let shift = 1 << (self.height() + 1);
-        let index = self.index() as i64 + shift * self.direction();
+        let index = self.in_order_index() as i64 + shift * self.direction();
         Self::from_in_order_index(index as u64)
     }
 
@@ -72,7 +100,7 @@ impl Position {
     /// The parent position has a height less 1 relative to this position.
     pub fn parent(self) -> Self {
         let shift = 1 << self.height();
-        let index = self.index() as i64 + shift * self.direction();
+        let index = self.in_order_index() as i64 + shift * self.direction();
         Self::from_in_order_index(index as u64)
     }
 
@@ -103,7 +131,7 @@ impl Position {
     /// |          11 |        1011 |         0100 |           2 |      2 |
     ///
     pub fn height(self) -> u32 {
-        (!self.index()).trailing_zeros()
+        (!self.in_order_index()).trailing_zeros()
     }
 
     // PRIVATE
@@ -129,7 +157,7 @@ impl Position {
     ///
     fn orientation(self) -> u8 {
         let shift = 1 << (self.height() + 1);
-        (self.index() & shift != 0) as u8
+        (self.in_order_index() & shift != 0) as u8
     }
 
     /// The "direction" to travel to reach the parent node.
