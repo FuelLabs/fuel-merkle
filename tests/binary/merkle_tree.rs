bae50902f845bce4ea8cdec4eb168b5a886a925a
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-10-08 14:54:17 -0400

    BMT `root` and `proof` tests

--- tests/binary/merkle_tree.rs
+++ tests/binary/merkle_tree.rs
@@ -2,21 +2,46 @@ use fuel_merkle::binary::MerkleTree;
 use fuel_merkle::common::StorageMap;
 use fuel_merkle_test_helpers::binary::MerkleTree as MockMerkleTree;
 
-#[test]
-fn test_prove() {
-    let mut reference_merkle_tree = MockMerkleTree::new();
-    reference_merkle_tree.set_proof_index(0);
+const MAX_LEAVES: u64 = 1 << 8;
+const TEST_DATA: std::ops::RangeInclusive<u64> = 0..=MAX_LEAVES;
 
+#[test]
+fn test_root() {
     let mut storage = StorageMap::new();
     let mut merkle_tree = MerkleTree::new(&mut storage);
-
-    let data = (0u32..10_000u32).into_iter();
-    for datum in data {
+    for datum in TEST_DATA {
         let _ = merkle_tree.push(&datum.to_be_bytes());
-        let _ = reference_merkle_tree.push(&datum.to_be_bytes());
     }
 
-    let reference_proof_set = reference_merkle_tree.prove().1;
-    let proof_set = merkle_tree.prove(0).unwrap().1;
-    assert_eq!(proof_set, reference_proof_set);
+    for _ in 0..MAX_LEAVES {
+        let mut reference_merkle_tree = MockMerkleTree::new();
+        for datum in TEST_DATA {
+            let _ = reference_merkle_tree.push(&datum.to_be_bytes());
+        }
+
+        let root = merkle_tree.root().unwrap();
+        let reference_root = reference_merkle_tree.root();
+        assert_eq!(root, reference_root);
+    }
+}
+
+#[test]
+fn test_prove() {
+    let mut storage = StorageMap::new();
+    let mut merkle_tree = MerkleTree::new(&mut storage);
+    for datum in TEST_DATA {
+        let _ = merkle_tree.push(&datum.to_be_bytes());
+    }
+
+    for index in 0..MAX_LEAVES {
+        let mut reference_merkle_tree = MockMerkleTree::new();
+        reference_merkle_tree.set_proof_index(index);
+        for datum in TEST_DATA {
+            let _ = reference_merkle_tree.push(&datum.to_be_bytes());
+        }
+
+        let proof_set = merkle_tree.prove(index).unwrap().1;
+        let reference_proof_set = reference_merkle_tree.prove().1;
+        assert_eq!(proof_set, reference_proof_set);
+    }
 }
