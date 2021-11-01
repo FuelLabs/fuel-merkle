d77365dcf1150946cfe32cf32bc83c9a39bb0a14
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-11-01 10:45:39 -0400

    Remove unused subtree code

--- src/sparse.rs
+++ src/sparse.rs
@@ -1,9 +1,7 @@
 mod hash;
 mod merkle_tree;
 mod node;
-mod subtree;
 
 pub(crate) use hash::{empty_sum, leaf_sum, node_sum, zero_sum};
 pub use merkle_tree::MerkleTree;
 pub(crate) use node::{Node, StorageNode};
-pub(crate) use subtree::Subtree;
