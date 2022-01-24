afaf028a99858821171c0c23c5915d93a7a21fb5
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-01-24 14:34:53 -0500

    Format after merge

--- src/sparse.rs
+++ src/sparse.rs
@@ -3,5 +3,5 @@ mod merkle_tree;
 mod node;
 
 pub(crate) use hash::{empty_sum, zero_sum};
-pub(crate) use node::{Buffer, Node, StorageNode};
 pub use merkle_tree::MerkleTree;
+pub(crate) use node::{Buffer, Node, StorageNode};
