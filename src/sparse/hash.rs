b8f9c210c5a04901be74ba6670735555e5ff5dbd
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-02-21 12:52:15 -0500

    Revert delete comment

--- src/sparse/hash.rs
+++ src/sparse/hash.rs
@@ -13,6 +13,8 @@ lazy_static! {
     static ref ZERO_SUM: Bytes32 = [0; 32];
 }
 
+// Merkle Tree hash of an empty list
+// MTH({}) = Hash()
 pub fn empty_sum() -> &'static Bytes32 {
     &*EMPTY_SUM
 }
