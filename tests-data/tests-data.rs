a8d19070ef7488aac7cc76e6dce1cfa827bd8157
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-04-13 17:40:25 -0400

    Data test stable

--- tests-data/tests-data.rs
+++ tests-data/tests-data.rs
@@ -1,11 +1,22 @@
-#![feature(custom_test_frameworks)]
-#![test_runner(datatest::runner)]
+use std::path::Path;
 
 use fuel_merkle::common::{Bytes32, StorageError, StorageMap};
 use fuel_merkle::sparse::MerkleTree;
 use serde::Deserialize;
 use std::convert::TryInto;
 use std::fmt::{Display, Formatter};
+
+use thiserror::Error;
+
+#[derive(Clone, Debug, Error)]
+pub enum TestError {
+    #[error("Test failed")]
+    Failed,
+    #[error("Unsupported action `{0}")]
+    UnsupportedAction(String),
+    #[error("Unsupported encoding `{0}")]
+    UnsupportedEncoding(String),
+}
 
 const BUFFER_SIZE: usize = 69;
 pub type Buffer = [u8; BUFFER_SIZE];
@@ -17,13 +28,15 @@ struct EncodedValue {
 }
 
 impl EncodedValue {
-    fn to_bytes(self) -> Vec<u8> {
+    fn to_bytes(self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         match self.encoding.as_str() {
-            "hex" => hex::decode(self.value).unwrap(),
-            "utf-8" => self.value.into_bytes(),
+            "hex" => Ok(hex::decode(self.value).unwrap()),
+            "utf-8" => Ok(self.value.into_bytes()),
 
             // Unsupported encoding
-            _ => panic!("unsupported encoding!"),
+            _ => Err(Box::<TestError>::new(TestError::UnsupportedEncoding(
+                self.encoding,
+            ))),
         }
     }
 }
@@ -36,23 +49,30 @@ struct Step {
 }
 
 impl Step {
-    pub fn execute(self, tree: &mut MerkleTree<StorageError>) {
+    pub fn execute(
+        self,
+        tree: &mut MerkleTree<StorageError>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         match self.action.as_str() {
             "update" => {
-                let key_bytes = self.key.unwrap().to_bytes();
+                let key_bytes = self.key.unwrap().to_bytes()?;
                 let key = &key_bytes.try_into().unwrap();
-                let data_bytes = self.data.unwrap().to_bytes();
+                let data_bytes = self.data.unwrap().to_bytes()?;
                 let data: &[u8] = &data_bytes;
                 tree.update(key, data).unwrap();
+                Ok(())
             }
             "delete" => {
-                let key_bytes = self.key.unwrap().to_bytes();
+                let key_bytes = self.key.unwrap().to_bytes()?;
                 let key = &key_bytes.try_into().unwrap();
                 tree.delete(key).unwrap();
+                Ok(())
             }
 
             // Unsupported action
-            _ => panic!("unsupported action!"),
+            _ => Err(Box::<TestError>::new(TestError::UnsupportedAction(
+                self.action,
+            ))),
         }
     }
 }
@@ -65,21 +85,20 @@ struct Test {
 }
 
 impl Test {
-    pub fn execute(self) {
+    pub fn execute(self) -> Result<(), Box<dyn std::error::Error>> {
         let mut storage = StorageMap::<Bytes32, Buffer>::new();
         let mut tree = MerkleTree::<StorageError>::new(&mut storage);
 
         for step in self.steps {
-            step.execute(&mut tree)
+            step.execute(&mut tree)?
         }
 
         let root = tree.root();
-        let expected_root: Bytes32 = {
-            let root_bytes = self.expected_root.to_bytes();
-            root_bytes.try_into().unwrap()
-        };
+        let expected_root: Bytes32 = self.expected_root.to_bytes()?.try_into().unwrap();
 
         assert_eq!(root, expected_root);
+
+        Ok(())
     }
 }
 
@@ -89,19 +108,10 @@ impl Display for Test {
     }
 }
 
-#[datatest::data("tests/smt_test_spec.yaml")]
-#[test]
-fn test_data(test: Test) {
+fn test(path: &Path) -> datatest_stable::Result<()> {
+    let data_file = std::fs::File::open(path).unwrap();
+    let test: Test = serde_yaml::from_reader(data_file).unwrap();
     test.execute()
 }
 
-/*#[test]
-fn test_data() {
-    let f = std::fs::File::open("tests/smt_test_spec.yaml").unwrap();
-    let tests: Vec<Test> = serde_yaml::from_reader(f).unwrap();
-    for test in tests {
-        println!("Executing test {}...", &test);
-        test.execute();
-    }
-}
-*/
+datatest_stable::harness!(test, "./tests-data/fixtures", r"^.*/*");
