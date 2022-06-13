e6f7550c416c75e5fab68827b2a7fcc49688224d
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-06-13 12:10:05 -0400

    In memory SMT tests

--- src/sparse/in_memory.rs
+++ src/sparse/in_memory.rs
@@ -41,6 +41,16 @@ impl<'a> MerkleTree<'a> {
         }
     }
 
+    pub fn delete(&mut self, key: &Bytes32) {
+        unsafe {
+            self.tree
+                .as_mut()
+                .unwrap_unchecked()
+                .delete(key)
+                .unwrap_unchecked();
+        }
+    }
+
     pub fn root(&self) -> Bytes32 {
         unsafe { self.tree.as_ref().unwrap_unchecked().root() }
     }
@@ -64,6 +74,56 @@ mod test {
         let mut tree = MerkleTree::new();
 
         tree.update(&sum(b"\x00\x00\x00\x00"), b"DATA");
+
+        let root = tree.root();
+        let expected_root = "39f36a7cb4dfb1b46f03d044265df6a491dffc1034121bc1071a34ddce9bb14b";
+        assert_eq!(hex::encode(root), expected_root);
+    }
+
+    #[test]
+    fn test_update_2() {
+        let mut tree = MerkleTree::new();
+
+        tree.update(&sum(b"\x00\x00\x00\x00"), b"DATA");
+        tree.update(&sum(b"\x00\x00\x00\x01"), b"DATA");
+
+        let root = tree.root();
+        let expected_root = "8d0ae412ca9ca0afcb3217af8bcd5a673e798bd6fd1dfacad17711e883f494cb";
+        assert_eq!(hex::encode(root), expected_root);
+    }
+
+    #[test]
+    fn test_update_3() {
+        let mut tree = MerkleTree::new();
+
+        tree.update(&sum(b"\x00\x00\x00\x00"), b"DATA");
+        tree.update(&sum(b"\x00\x00\x00\x01"), b"DATA");
+        tree.update(&sum(b"\x00\x00\x00\x02"), b"DATA");
+
+        let root = tree.root();
+        let expected_root = "52295e42d8de2505fdc0cc825ff9fead419cbcf540d8b30c7c4b9c9b94c268b7";
+        assert_eq!(hex::encode(root), expected_root);
+    }
+
+    #[test]
+    fn test_update_1_delete_1() {
+        let mut tree = MerkleTree::new();
+
+        tree.update(&sum(b"\x00\x00\x00\x00"), b"DATA");
+        tree.delete(&sum(b"\x00\x00\x00\x00"));
+
+        let root = tree.root();
+        let expected_root = "0000000000000000000000000000000000000000000000000000000000000000";
+        assert_eq!(hex::encode(root), expected_root);
+    }
+
+    #[test]
+    fn test_update_2_delete_1() {
+        let mut tree = MerkleTree::new();
+
+        tree.update(&sum(b"\x00\x00\x00\x00"), b"DATA");
+        tree.update(&sum(b"\x00\x00\x00\x01"), b"DATA");
+        tree.delete(&sum(b"\x00\x00\x00\x01"));
 
         let root = tree.root();
         let expected_root = "39f36a7cb4dfb1b46f03d044265df6a491dffc1034121bc1071a34ddce9bb14b";
