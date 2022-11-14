02463a91c8b74cc965330825663663b57d297047
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-11-14 11:30:02 -0500

    Manual from prefix error

--- src/common/error.rs
+++ src/common/error.rs
@@ -4,5 +4,11 @@ use crate::common::PrefixError;
 #[cfg_attr(feature = "std", derive(thiserror::Error))]
 pub enum DeserializeError {
     #[cfg_attr(feature = "std", error(transparent))]
-    PrefixError(#[from] PrefixError),
+    PrefixError(PrefixError),
+}
+
+impl From<PrefixError> for DeserializeError {
+    fn from(err: PrefixError) -> Self {
+        DeserializeError::PrefixError(err)
+    }
 }
