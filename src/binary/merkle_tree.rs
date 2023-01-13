5294a32692dd69d260054f15fc0d25915328a04a
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-01-12 23:21:12 -0500

    revert

--- src/binary/merkle_tree.rs
+++ src/binary/merkle_tree.rs
@@ -324,7 +324,7 @@ mod test {
         binary::{empty_sum, leaf_sum, node_sum, Node},
         common::StorageMap,
     };
-    use fuel_merkle_test_helpers::*;
+    use fuel_merkle_test_helpers::TEST_DATA;
     use fuel_storage::StorageInspect;
 
     use alloc::vec::Vec;
